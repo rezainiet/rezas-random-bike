4e252ccffafa3b291ab0faee3b6f82e9bd589718
rezainiet <masudrezaog@gmail.com>
2022-03-26 23:16:49 +0600

    Product component completed

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Product.css'
-
-const Product = (props) => {
-    const { name, id, picture } = props.product;
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+const Product = ({ product, clickHandelar }) => {
+    const { picture, name, balance } = product
     return (
-        <div className='product-container'>
-            <img src={picture} alt="" />
-            <h3>{name}</h3>
-            <button className="btn-cart">
-                <p>Add To Cart</p>
-            </button>
+        <div className='product-info shadow-lg'>
+            <img className='img-fluid' src={picture} alt="" />
+            <div className='px-2'>
+                <h5>Bike Name: {name}</h5>
+                <p>Price: {balance}</p>
+            </div>
+            <button className='button' onClick={() => { clickHandelar(product) }} >Add To Cart <FontAwesomeIcon className='text-black fs-5' icon={faShoppingCart} /></button>
         </div>
     );
 };
